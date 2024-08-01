93573a58c7c1401d7ea48df3263e8c72e933e813
Madino <89445570+Maldin0@users.noreply.github.com>
2024-08-01 16:33:14 +0700

    week-5

---
+++ week-5/week5_tutorial/companies/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from datetime import date
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    ticker = models.CharField(max_length=20, null=True)
+    num_employees = models.IntegerField()
+    num_tables = models.IntegerField()
+    num_chairs = models.IntegerField()
+
+    def __str__(self):
+        return self.name
